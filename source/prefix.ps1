731d96b9db2385211c7b7fcec280ab173d0c7a86
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-11-14 21:02:14 +0100

    fix: trigger pipeline

--- source/prefix.ps1
+++ source/prefix.ps1
@@ -1,1 +1,1 @@
-﻿# The content of this file will be prepended to the top of the psm1 module file. This is useful for custom module setup is needed on import.
+﻿#  The content of this file will be prepended to the top of the psm1 module file. This is useful for custom module setup is needed on import.
